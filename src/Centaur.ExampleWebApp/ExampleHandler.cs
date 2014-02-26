c18bc57082a4d518455a55b12404d270cd09d85d
Josh Chisholm <josh@featurist.co.uk>
2014-02-26 12:16:34 +0000

    add example web app and test projects

---
+++ src/Centaur.ExampleWebApp/ExampleHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+
+namespace Centaur.ExampleWebApp
+{
+    public class ExampleHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("hello!");
+            context.Response.End();
+        }
+
+        public bool IsReusable { get { return true; } }
+    }
+}
---
+++ src/Centaur.Tests/ExampleWebAppTest.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Net;
+using NUnit.Framework;
+
+namespace Centaur.Tests
+{
+    [TestFixture]
+    public class ExampleWebAppTest
+    {
+        private IISExpressHost _host;
+
+        [SetUp]
+        public void StartHost()
+        {
+            _host = new IISExpressHost("../../../Centaur.ExampleWebApp/", 9059);
+            _host.Start();
+        }
+
+        [TearDown]
+        public void StopHost()
+        {
+            _host.Stop();
+        }
+
+        [Test]
+        public void IisExpressHostedWebAppRespondsToRequests()
+        {
+            Assert.That(Get("http://localhost:9059"), Is.EqualTo("hello!"));
+        }
+
+        static string Get(string url)
+        {
+            var request = WebRequest.Create(url);
+            using (var response = request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    if (responseStream == null) return null;
+                    var reader = new StreamReader(responseStream);
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
